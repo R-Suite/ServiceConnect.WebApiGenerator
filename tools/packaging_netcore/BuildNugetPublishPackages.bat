d04be3ab5233b82db0ec9b89f98d48bc5bfc6ee2
Jakub Pachansky <jpachansky@gmail.com>
2018-03-15 15:15:14 +0000

    Clean up

--- tools/packaging_netcore/BuildNugetPublishPackages.bat
+++ tools/packaging_netcore/BuildNugetPublishPackages.bat
@@ -11,7 +11,7 @@ del *.nupkg
 dotnet pack "%OUTDIR%ServiceConnect.WebApiGenerator\ServiceConnect.WebApiGenerator.csproj" -c Release /p:PackageVersion=1.0.5
 ::NuGet pack "%OUTDIR%ServiceConnect.WebApiGenerator\ServiceConnect.WebApiGenerator.nuspec"
 
-nuget push %OUTDIR%ServiceConnect.WebApiGenerator\bin\Release\ServiceConnect.WebApiGenerator.1.0.5.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push %OUTDIR%ServiceConnect.WebApiGenerator\bin\Release\ServiceConnect.WebApiGenerator.1.0.5.nupkg -Source https://www.nuget.org/api/v2/package
 
 @ECHO === === === === === === === ===
 
